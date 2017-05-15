4c54d8fed69170b2320e3d7f11f44758373d1b1b
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-05-15 18:44:24 -0500

    completed project

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,18 @@
-console.log("shrroooomms");
-
 //FIREBASE PROMISE FUNCTION
 
 app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+
+  $scope.shrooms = [];
+  $scope.poison = null;
+  $scope.searchText;
 
 
 let getItemList = () => {
   let itemz = [];
   return $q((resolve, reject) => {
-    $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+    $http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
     .then((fbItems) => {
+      console.log(fbItems);
         let itemCollection = fbItems.data;
         Object.keys(itemCollection).forEach((key) => {
           itemCollection[key].id=key;
@@ -26,7 +29,7 @@ let getItemList = () => {
 
   let getItems = () => {
     getItemList().then((itemz) => {
-      // $scope.items = itemz;
+      $scope.shrooms = itemz;
       console.log("itemz", itemz);
     }).catch((error) => {
       console.log("get error", error);
